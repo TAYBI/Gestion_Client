e07ae75e5a9d512cc68973b3447e250c4a16f545
TAYBI <taybib4@gmail.com>
2020-06-02 11:49:14 -0700

    first commit && create databse && tables

---
+++ SQLQuery.sql
@@ -0,0 +1,24 @@
+create database Gestion_Client
+use Gestion_Client
+
+create table Service (
+	Code varchar(20) primary key,
+	libellé varchar(50)
+)
+
+create table Employe (
+	Matricule varchar(20) primary key,
+	Nom varchar(50), 
+	Prénom varchar(50), 
+	Date_naissance Date, 
+	grade varchar(50), 
+	échelle varchar(50), 
+	code_service varchar(20) foreign key references Service(Code)
+) 
+
+create table Conge (
+	Matricule varchar(20) foreign key references Employe(Matricule), 
+	Date_congé Date, 
+	Type_congé varchar(20),
+	Durée int
+)
